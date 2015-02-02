92751a5705a3d9701ab1a7f42b30f900e5cd3d21
ChrisBurgess94 <b2034227@my.shu.ac.uk>
2015-02-02 11:57:24 +0000

    Implemented scaffolding and bootstrapping

---
+++ StudentEnrol/grails-app/conf/BootStrap.groovy
@@ -0,0 +1,29 @@
+import com.studentenrol.*
+
+class BootStrap {
+
+    def init = { servletContext ->
+
+      def course1 = new Course(title:'Interactive Media with Animation',
+                                       
+                                       department: 'Computing',
+
+                                       description:'"Blah Blah Blah blah."',
+
+                                       leader: 'Joe Jojo',
+ 
+                                       code: 'IMA101',
+
+                                       startDate: new Date('19/01/2015'),
+
+                                       endDate: new Date('19/01/2020')).save()
+
+
+
+
+             
+}
+
+    def destroy = {
+    }
+}
